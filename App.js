2d24b81954edb2dec3c728bc563bd118afee26a7
unknown <ezkyuhiro24@gmail.com>
2023-11-22 01:14:48 +0800

    import homescreen and other stuffs to separate post and get feature

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import * as Location from 'expo-location';
+import MapComponent from './component/MapComponent';
 
 export default function App() {
   const [location, setLocation] = useState(null);
+  const [showMap, setShowMap] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -21,7 +23,7 @@ export default function App() {
   const sendLocation = async () => {
     if (location) {
       const { latitude, longitude } = location.coords;
-      
+
       try {
         const response = await fetch('http://192.168.254.113:3000/api/send-location', {
           method: 'POST',
@@ -31,29 +33,34 @@ export default function App() {
           body: JSON.stringify({ latitude, longitude }),
         });
 
-        if (!response.ok) {
-          throw new Error('Network request failed');
-        }
-      
         const data = await response.json();
         console.log(data.message);
+
+        // After sending location, show the map
+        setShowMap(true);
       } catch (error) {
         console.error('Error sending location:', error.message);
       }
     } else {
       console.log('Location not available yet');
     }
-  };  
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Disaster Location App</Text>
-      {location && (
-        <Text>
-          Latitude: {location.coords.latitude}, Longitude: {location.coords.longitude}
-        </Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={sendLocation}>
+          <Text style={styles.buttonText}>Send Location</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => setShowMap(!showMap)}>
+          <Text style={styles.buttonText}>{showMap ? 'Hide Map' : 'Show Map'}</Text>
+        </TouchableOpacity>
+      </View>
+      {showMap && (
+        <View style={styles.mapContainer}>
+          <MapComponent />
+        </View>
       )}
-      <Button title="Send Location" onPress={sendLocation} />
     </View>
   );
 }
@@ -64,5 +71,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '50%',
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 20,
+    margin: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  mapContainer: {
+    flex: 1,
+    width: '100%',
   },
 });
