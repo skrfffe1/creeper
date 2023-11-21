9f26b23db8d15e7d7fd3b1d63ec86927ab9f4857
unknown <ezkyuhiro24@gmail.com>
2023-11-21 21:36:14 +0800

    testing geolocation mockup

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  const sendLocation = () => {
+    // Send the location data to your server or do something with it
+    if (location) {
+      console.log('Sending location:', location.coords);
+      // Add logic to send location data to your server
+    } else {
+      console.log('Location not available yet');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <Text>Disaster Location App</Text>
+      {location && (
+        <Text>
+          Latitude: {location.coords.latitude}, Longitude: {location.coords.longitude}
+        </Text>
+      )}
+      <Button title="Send Location" onPress={sendLocation} />
     </View>
   );
 }
@@ -13,9 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  map: {
-    width: '100%',
-    height: '100%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
